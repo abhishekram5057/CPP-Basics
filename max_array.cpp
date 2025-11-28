54291e3e8b77487a48550e35f9946ff6c24a01b7
abhishekram5057 <abhishekram5057@gmail.com>
2025-11-28 19:39:23 +0530

    Add files via upload

---
+++ max_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+
+    cout << "Maximum value = " << maxVal << endl;
+
+    return 0;
+}
